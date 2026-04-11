8d4aad1f02018972c30666b9dd5433c6da583b92
Yuliia Sidak <yuliaxrula@gmail.com>
2026-04-11 15:18:28 +0200

    fixed minor issues

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid}`;
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} creditss`;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
